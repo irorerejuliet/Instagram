0565f583c01dc7c23188e1b710536b535c8783a3
irorere juliet <irorerejuliet@gmail.com>
2026-06-08 20:02:47 +0100

    Profile page  still showing 404

--- src/app/api/profile/[username]/route.ts
+++ src/app/api/profile/[username]/route.ts
@@ -1,3 +1,31 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
 export async function GET(
-    reguest: Request
-)
+  request: Request,
+  { params }: { params: { username: string } },
+) {
+  const { username } = params;
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("username", username)
+    
+
+  if (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: error.message,
+      },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    profile: data,
+  });
+}
--- src/features/profileSection/Profile.tsx
+++ src/features/profileSection/Profile.tsx
@@ -1,13 +1,24 @@
 import ProfileDetails from "@/components/ProfileDetails";
 import MobileProfile from "../mobile/MobileProfile";
 
-const Profile = () => {
+
+type ProfileProps = {
+  id: string;
+  username: string;
+  full_name: string | null;
+  bio: string | null;
+  avatar_url: string | null;
+  email: string;
+};
+
+
+const Profile = ({profile}: {profile: ProfileProps}) => {
   return (
     <div className="w-full min-h-screen lg:py-10 md:py-6 sm:py-4 lg:px-14 md:12 smpx-7 px-3">
       {/* profile for big screem */}
-      <ProfileDetails />
+      <ProfileDetails profile={profile} />
       {/* profile for larfe screen */}
-      <MobileProfile/>
+      <MobileProfile />
     </div>
   );
 };
